1222f83d39facc3b6065c8f2138194397f10950d
Macbook <trieungochai.dev@gmail.com>
2021-01-12 18:11:20 +0700

    Making HTTP Requests

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
- 
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596';
+
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
