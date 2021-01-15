199c19baf7619c5328cc7e741adbf680e412fb5d
Macbook <trieungochai.dev@gmail.com>
2021-01-15 18:37:58 +0700

    Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596';
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.current.weather_descriptions[0] + ': It is currently ' + response.body.current.temperature + ' degrees out. It feel likes ' + response.body.current.feelslike + ' degrees out.');
 });
