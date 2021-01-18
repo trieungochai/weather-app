18beb7bff82601e3f1051df050471eceb68a1489
Macbook <trieungochai.dev@gmail.com>
2021-01-18 20:18:48 +0700

    Destructuring and Property Shorthand Challenge

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + longitude + '&units=f';
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to weather services', undefined);
-    } else if (response.body.error) {
-      callback('Unable to find location', undefined);
+        callback('Unable to connect to weather services', undefined);
+    } else if (body.error) {
+        callback('Unable to find location', undefined);
     } else {
-      callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.");
+        callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.");
     };
-  })
+  });
 };
 
 module.exports = forecast;
